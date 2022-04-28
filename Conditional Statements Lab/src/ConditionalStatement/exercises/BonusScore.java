4a672a0169798fb44e12f404ae26574f0848ecff
Valentin Tsonevski <104366614+ValentinTsonevski@users.noreply.github.com>
2022-04-28 22:18:49 +0100

    Add files via upload

---
+++ Conditional Statements Lab/src/ConditionalStatement/exercises/BonusScore.java
@@ -0,0 +1,26 @@
+package ConditionalStatement.exercises;
+
+import java.util.Scanner;
+
+public class BonusScore {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+        double bonus = 0.0;
+        if (number <= 100) {
+            bonus = 5;
+        } else if (number <= 1000) {
+            bonus = number * 0.20;
+        } else if (number > 1000) {
+            bonus = number * 0.10;
+        }
+        if (number % 2 == 0) {
+            bonus += 1;
+        } else if (number % 10 == 5) {
+            bonus += 2;
+        }
+        System.out.println(bonus);
+        System.out.println(number + bonus);
+
+    }
+}
