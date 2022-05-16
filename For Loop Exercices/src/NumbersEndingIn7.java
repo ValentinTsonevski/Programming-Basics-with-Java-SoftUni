1b8fea637fac1edfed36ae02ad4a24a79803026a
Valentin Tsonevski <104366614+ValentinTsonevski@users.noreply.github.com>
2022-05-16 16:15:59 +0100

    Add files via upload

---
+++ For Loop Exercices/src/NumbersEndingIn7.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class NumbersEndingIn7 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 1000 ; i++) {
+       if(i % 10 == 7){
+           System.out.println(i);
+       }
+        }
+
+
+
+
+    }
+}
