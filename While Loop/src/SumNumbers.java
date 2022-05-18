0549e0486eba3fa0a9c8fb63b80a0f006f5deb5f
Valentin Tsonevski <104366614+ValentinTsonevski@users.noreply.github.com>
2022-05-18 22:27:55 +0100

    Add files via upload

---
+++ While Loop/src/SumNumbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int firstNumber = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+        while (sum < firstNumber){
+         int number = Integer.parseInt(scanner.nextLine());
+
+            sum = sum + number;
+        }
+        System.out.println(sum);
+
+
+
+
+    }
+}
