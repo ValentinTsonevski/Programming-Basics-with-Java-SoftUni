ae66276e0af250b35a475f3288f42245722f821d
Valentin Tsonevski <104366614+ValentinTsonevski@users.noreply.github.com>
2022-05-23 00:22:10 +0100

    Add files via upload

---
+++ While Loop - Exercise/src/Moving.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Moving {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int width = Integer.parseInt(scanner.nextLine());
+        int length = Integer.parseInt(scanner.nextLine());
+        int height = Integer.parseInt(scanner.nextLine());
+        int volume = width * length * height ;
+        String command = scanner.nextLine();
+
+        while (!command.equals("Done")){
+        int boxes = Integer.parseInt(command);
+        volume -= boxes;
+        if(volume <= 0){
+            break;
+        }
+
+
+         command = scanner.nextLine();
+        }
+        if(volume <= 0){
+            System.out.printf("No more free space! You need %d Cubic meters more.", Math.abs(volume));
+        }else{
+            System.out.printf("%d Cubic meters left.", volume);
+        }
+
+
+
+    }
+}
